d0e7aeda99e3f4399ff164bc31da710fe2a74a09
RuanDevz <Ruanbatista1509@outlook.com>
2024-04-26 20:49:19 -0300

    Adicionado funcao de desabilitar

--- pizzaria/src/components/button/Button.jsx
+++ pizzaria/src/components/button/Button.jsx
@@ -3,10 +3,10 @@ import './buttonstyle.css'
 
 
 // eslint-disable-next-line react/prop-types
-const Button = ({children, onClick}) => {
+const Button = ({children, onClick, disabled}) => {
     return (
       <div>
-      <button onClick={onClick} className="pushable">
+      <button disabled={disabled} onClick={onClick} className="pushable">
       <span className="shadow"></span>
       <span className="edge"></span>
       <span className="front">
--- pizzaria/src/pages/Register/Register.jsx
+++ pizzaria/src/pages/Register/Register.jsx
@@ -1,11 +1,11 @@
+/* eslint-disable react/no-children-prop */
 import React, { useEffect, useState } from 'react';
-import { RegisterContainer, SucessMessage,Containerinputs } from './RegisterStyle.js';
+import { RegisterContainer, SucessMessage, Containerinputs } from './RegisterStyle.js';
 import Input from '../../components/Inputs/Input.jsx';
 import Button from '../../components/button/Button.jsx';
 import Correto from '../../assets/Correto.png';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import Headerstart from '../../components/Headerstart/Headerstart.jsx';
 import { IoMdArrowRoundBack } from "react-icons/io";
 
 const Register = () => {
@@ -14,6 +14,7 @@ const Register = () => {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState('');
   const [isRegistered, setIsRegistered] = useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false); // Estado do botão
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -32,24 +33,26 @@ const Register = () => {
     if (username.length < 4) {
       setError('Seu nome precisa ter pelo menos 4 caracteres');
       isValid = false;
-      return
+      return;
     }
 
     if (password.length <= 8) {
       setError('Sua senha precisa ter mais de 8 caracteres');
       isValid = false;
-      return
+      return;
     }
 
     if (password !== confirmPassword) {
       setError('As senhas não coincidem');
       isValid = false;
-      return
+      return;
     }
 
     if (!isValid) {
-      return; 
+      return;
     }
+
+    setIsButtonDisabled(true);
 
     axios
       .post('https://backendpizzaria.onrender.com/user/register', {
@@ -57,17 +60,18 @@ const Register = () => {
         password: password,
       })
       .then((response) => {
-        if(response.data.error){
-          setError(response.data.error)
-          return
+        if (response.data.error) {
+          setError(response.data.error);
+          setIsButtonDisabled(false);
+          return;
         }
         setIsRegistered(true);
       })
       .catch((error) => {
         console.error('Erro ao cadastrar usuário:', error);
+        setIsButtonDisabled(false); 
       });
   };
-
 
   return (
     <RegisterContainer>
@@ -81,38 +85,39 @@ const Register = () => {
       ) : (
         <form>
           <Containerinputs>
-          <Input
-            type='text'
-            placeholder='Nome'
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            htmlFor='username'
-            id='username'
-          />
+            <Input
+              type='text'
+              placeholder='Nome'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              htmlFor='username'
+              id='username'
+            />
 
-          <Input
-            type='password'
-            placeholder='Sua senha'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            htmlFor='password'
-            id='password'
-          />
+            <Input
+              type='password'
+              placeholder='Sua senha'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              htmlFor='password'
+              id='password'
+            />
 
-          <Input
-            type='password'
-            placeholder='Confirmar senha'
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            htmlFor='confirmPassword'
-            id='confirmPassword'
-          />
+            <Input
+              type='password'
+              placeholder='Confirmar senha'
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              htmlFor='confirmPassword'
+              id='confirmPassword'
+            />
           </Containerinputs>
           {error && <p id='error'>{error}</p>}
-
         </form>
       )}
-      {!isRegistered && <Button children='Cadastrar' onClick={handleClick} />}
+      {!isRegistered && (
+        <Button children='Cadastrar' onClick={handleClick} disabled={isButtonDisabled} />
+      )}
     </RegisterContainer>
   );
 };
